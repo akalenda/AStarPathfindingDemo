19ce72d6fbbbb558ab8c03e635c3de0ccfe9c080
Andrew Kalenda <Andrew Kalenda>
2015-12-16 16:33:50 -0800

    Moved to public repo

---
+++ WpfApplication6/Astar.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstarDemo
+{
+    class Astar
+    {
+        Node currNode;
+        Node[,] map;
+        Int32 width;
+        Int32 height;
+        Int32 strtX;
+        Int32 strtY;
+        Int32 currX;
+        Int32 currY;
+        Int32 destX;
+        Int32 destY;
+        public delegate void DataFeed(Int32 x, Int32 y, Char type, Int32 data, Char direction);
+        DataFeed callback;
+        List<Node> openList;
+        
+        // CONSTRUCTOR; array must match the given dimensions, and is used to provide
+        // the cost of traversing points on the map. Use Int32.MaxValue for impassible points.
+        public Astar(Int32 width, Int32 height, Int32[,] traversalCosts)
+        {
+            this.width = width;
+            this.height = height;
+            map = new Node[width, height];
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    map[x, y] = new Node(x, y, traversalCosts[x, y]);
+            openList = new List<Node>();
+        }
+
+        // Remove the given node from the open list and mark it closed
+        private void close(Node n)
+        {
+            n.isOpen = false;
+            n.isClosed = true;
+            openList.Remove(n);
+        }
+
+        // Sets the algorithm to run from a new start to a new end position
+        // The DataFeed callback lets the algorithm (optionally) provide feedback to the host program
+        public void navFromTo(Int32 startX, Int32 startY, Int32 destinationX, Int32 destinationY, DataFeed callback)
+        {
+            // Reset old nav data
+            foreach (Node n in map)
+            {
+                n.isClosed = false;
+                n.isOpen = false;
+                n.parent = null;
+            }
+            
+            // Save new navigation data
+            strtX = startX;
+            strtY = startY;
+            destX = destinationX;
+            destY = destinationY;
+            this.callback = callback;
+            
+            // Add starting node to open_list
+            open(map[strtX, strtY], null, ' ');
+        }
+
+        // Add the given node to the open list with the given parent
+        private void open(Node n, Node parent, Char direction)
+        {
+            if (n.isOpen || n.isClosed)
+                return;
+            if (n.traversalCost == Int32.MaxValue)
+            {
+                n.etaCost = Int32.MaxValue;
+                n.totalCost = Int32.MaxValue;
+                n.isClosed = true;
+                return;
+            }
+            n.parent = parent;
+            if (parent != null)
+                n.arrivalCost = parent.arrivalCost + parent.traversalCost;
+            n.etaCost = Math.Abs(destX - currX) + Math.Abs(destY - currY) + n.traversalCost;
+            n.totalCost = n.arrivalCost + n.etaCost;
+            openList.Add(n);
+            n.isOpen = true;
+            callback(n.x, n.y, 'o', n.totalCost, direction);
+        }
+
+        // Progresses the algorithm forward one step
+        // Returns a linked list of x-y value pairs delineating a complete path if it is found
+        // Returns an empty linked list if no such path is possible
+        // Returns null if there are steps remaining
+        public LinkedList<Int32[]> step()
+        {
+            // While there's stuff in the open list
+            if (0 < openList.Count)
+            {
+                // Set current node to be the lowest-cost node from open list
+                openList.Sort();
+                currNode = openList[0];
+                currX = currNode.x;
+                currY = currNode.y;
+
+                // Is it the goal?
+                if (currX == destX && currY == destY)
+                {
+                    // Path is complete
+                    LinkedList<Int32[]> path = new LinkedList<Int32[]>();
+                    Int32[] point;
+                    currNode = currNode.parent;
+                    while (currNode.parent != null)
+                    {
+                        point = new Int32[2];
+                        point[0] = currNode.x;
+                        point[1] = currNode.y;
+                        path.AddFirst(point);
+                        currNode = currNode.parent;
+                    }
+                    return path;
+                }
+                else
+                {
+                    // Close current node
+                    close(currNode);
+                    callback(currX, currY, 'c', currNode.totalCost, ' ');
+
+                    // Open each adjacent node
+                    if (currNode.y > 0) // north
+                        open(map[currNode.x, currNode.y - 1], currNode, '^');
+                    if (currNode.x + 1 < width) // east
+                        open(map[currNode.x + 1, currNode.y], currNode, '>');
+                    if (currNode.y + 1 < height) // south
+                        open(map[currNode.x, currNode.y + 1], currNode, 'v');
+                    if (currNode.x > 0) // west
+                        open(map[currNode.x - 1, currNode.y], currNode, '<');
+                }
+                return null;
+            }
+            // Unable to find a path!
+            return new LinkedList<Int32[]>();
+        }
+
+        // Alters the traversal cost of the given point
+        public void updateTraversal(Int32 x, Int32 y, Int32 newCost)
+        {
+            map[x, y].traversalCost = newCost;
+        }
+
+        // Convenience class containing all the data pertinent to a particular position on the grid
+        public class Node : IComparable
+        {
+            public Boolean isOpen;
+            public Boolean isClosed;
+            public Node parent;
+            public Int32 arrivalCost;
+            public Int32 traversalCost;
+            public Int32 etaCost;
+            public Int32 totalCost;
+            public Int32 x;
+            public Int32 y;
+
+            // CONSTRUCTOR
+            public Node(Int32 x, Int32 y, Int32 travelCost)
+            {
+                this.x = x;
+                this.y = y;
+                this.traversalCost = travelCost;
+                isClosed = false;
+                isOpen = false;
+            }
+
+            public int Compare(object o1, object o2)
+            {
+                Node n1 = (Node)o1;
+                Node n2 = (Node)o2;
+
+                if (n1.totalCost < n2.totalCost)
+                    return -1;
+                if (n1.totalCost > n2.totalCost)
+                    return 1;
+                return 0;
+            }
+
+            public int CompareTo(object o)
+            {
+                Node n = (Node)o;
+
+                if (totalCost < n.totalCost)
+                    return -1;
+                if (totalCost > n.totalCost)
+                    return 1;
+                return 0;
+            }
+        }
+    }
+}
---
+++ WpfApplication6/MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿/* San Jose State University
+ * CS 134 - Game Design and Programming
+ * Instructor: Sun Tee Teoh
+ * Student: Andrew Kalenda
+ * 
+ * A* Algorithm Demo
+ * 
+ * There is a minor bug where overwriting a start/goal tile with another
+ * tile does not remove the start/goal information
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AstarDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int HEIGHT = 16;
+        int WIDTH = 16;
+        Tile[,] tileAt;
+        Brush currColor;
+        Int32 currCost;
+        Astar astar;
+        Boolean pathNotSet;
+        Int32 startX;
+        Int32 startY;
+        Int32 goalX;
+        Int32 goalY;
+        delegate void PaintFunction(Tile tile);
+        PaintFunction extraStep;
+        PaintFunction doNothing;
+        PaintFunction changeStart;
+        PaintFunction changeGoal;
+        Astar.DataFeed paintAstar;
+        LinkedList<Int32[]> path;
+
+        // MAIN FUNCTION: set up the GUI
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set up the grid
+            for (int x = 0; x < WIDTH; x++)
+                bttnGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            for (int y = 0; y < HEIGHT; y++)
+                bttnGrid.RowDefinitions.Add(new RowDefinition());
+
+            // Fill it with buttons
+            Thickness noBorder = new Thickness(0);
+            tileAt = new Tile[HEIGHT, WIDTH];
+            for (int x = 0; x < WIDTH; x++)
+            {
+                for (int y = 0; y < HEIGHT; y++)
+                {
+                    tileAt[x, y] = new Tile(); 
+                    tileAt[x, y].x = x;
+                    tileAt[x, y].y = y;
+                    tileAt[x, y].BorderThickness = noBorder; 
+                    tileAt[x, y].Click += setTile; // hook the paint function below so it is invoked on click
+                    bttnGrid.Children.Add(tileAt[x, y]);
+                    Grid.SetColumn(tileAt[x, y], x);
+                    Grid.SetRow(tileAt[x, y], y);
+                }
+            }
+
+            // Initialize the pathfinding data
+            reset();
+
+            // DELEGATES
+            // Set up some functional programming for the paint callbacks
+            extraStep = doNothing = delegate(Tile tile) { };
+            changeStart = delegate(Tile tile)
+            {
+                if (startX != -1)
+                {
+                    tileAt[startX, startY].Background = Brushes.White;
+                    astar.updateTraversal(startX, startY, 1);
+                }
+                startX = tile.x;
+                startY = tile.y;
+            };
+            changeGoal = delegate(Tile tile)
+            {
+                if (goalX != -1)
+                {
+                    tileAt[goalX, goalY].Background = Brushes.White;
+                    astar.updateTraversal(goalX, goalY, 1);
+                }
+                goalX = tile.x;
+                goalY = tile.y;
+            };
+            paintAstar = delegate(Int32 x, Int32 y, Char type, Int32 data, Char direction)
+            {
+                switch (type)
+                {
+                    case 'o':
+                        if (tileAt[x, y].Background.Equals(Brushes.White))
+                            tileAt[x, y].Background = Brushes.Beige;
+                        if (tileAt[x, y].Background.Equals(Brushes.Blue))
+                            tileAt[x, y].Background = Brushes.Teal;
+                        tileAt[x, y].Content = direction + " " + data;
+                        break;
+                    case 'c':
+                        if (tileAt[x, y].Background.Equals(Brushes.White))
+                            tileAt[x, y].Background = Brushes.DarkGoldenrod;
+                        if (tileAt[x, y].Background.Equals(Brushes.Blue))
+                            tileAt[x, y].Background = Brushes.DarkOrange;
+                        break;
+                    default:
+                        break;
+                }
+            };
+        }
+
+        // BUTTON FUNCTIONS: reset
+        public void reset()
+        {
+            Int32[,] costFor = new Int32[WIDTH, HEIGHT];
+            pathNotSet = true;
+            startX = -1;
+            startY = -1;
+            goalX = -1;
+            goalY = -1;
+
+            for (int x = 0; x < WIDTH; x++)
+            {
+                for (int y = 0; y < HEIGHT; y++)
+                {
+                    costFor[x, y] = 1;
+                    tileAt[x, y].Background = Brushes.White;
+                    tileAt[x, y].Content = "";
+                }
+            }
+            astar = new Astar(WIDTH, HEIGHT, costFor);
+        }
+        public void reset(Object bttn, EventArgs e)
+        {
+            reset();
+        }
+
+        // BUTTON FUNCTIONS: painting tiles
+        public void setPaint_Free(Object bttn, EventArgs e)
+        {
+            currColor = Brushes.White;
+            currCost = 1;
+            extraStep = doNothing;
+        }
+        public void setPaint_Goal(Object bttn, EventArgs e)
+        {
+            currColor = Brushes.Red;
+            currCost = 1;
+            extraStep = changeGoal;
+        }
+        public void setPaint_Strt(Object bttn, EventArgs e)
+        {
+            currColor = Brushes.Green;
+            currCost = 1;
+            extraStep = changeStart;
+        }
+        public void setPaint_Wall(Object bttn, EventArgs e)
+        {
+            currColor = Brushes.DarkGray;
+            currCost = Int32.MaxValue;
+            extraStep = doNothing;
+        }
+        public void setPaint_Watr(Object bttn, EventArgs e)
+        {
+            currColor = Brushes.Blue;
+            currCost = 8;
+            extraStep = doNothing;
+        }
+        public void setTile(Object bttn, EventArgs e)
+        {
+            Tile tile = (Tile)bttn;
+            tile.Background = currColor;
+            tile.Content = "";
+            astar.updateTraversal(tile.x, tile.y, currCost);
+            extraStep(tile);
+        }
+
+        // PATHING FUNCTIONS
+        public void setPath()
+        {
+            // Have the start and end points not been selected yet?
+            if (startX == -1 || goalX == -1)
+                return;
+
+            astar.navFromTo(startX, startY, goalX, goalY, paintAstar);
+            pathNotSet = false;
+        }
+
+
+        // BUTTON FUNCTIONS: stepping
+        public void stepNext(Object bttn, EventArgs e)
+        {
+            if (pathNotSet)
+            {
+                setPath();
+                return;
+            }
+            if ((path = astar.step()) != null)
+                foreach (Int32[] pair in path)
+                    tileAt[pair[0], pair[1]].Background = Brushes.Yellow;
+        }
+        public void stepFnsh(Object bttn, EventArgs e)
+        {
+            if (pathNotSet)
+            {
+                setPath();
+                return;
+            }
+            while ((path = astar.step()) == null) ;
+            foreach (Int32[] pair in path)
+                tileAt[pair[0], pair[1]].Background = Brushes.Yellow;
+        }
+        
+        // NESTED CONVENIENCE CLASS
+        public class Tile : Button
+        {
+            public Int32 x;
+            public Int32 y;
+        }
+    }
+}
